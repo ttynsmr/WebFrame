7e45a5f1c1562dd8b4f1ef0f7954c060771b7c7e
Tetsuya Nishimura <ttynsmr@gmail.com>
2022-04-16 13:34:48 +0900

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace WebFrame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public string[] CommandLineArgs { get; private set; } = Array.Empty<string>();
+
+        private App()
+        {
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            CommandLineArgs = e.Args;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace WebFrame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Uri uri = new("https://duckduckgo.com");
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            App app = (App)Application.Current;
+            if (app.CommandLineArgs.Length >= 1)
+            {
+                uri = new Uri(app.CommandLineArgs[0]);
+            }
+
+            InitializeAsync();
+        }
+
+        async void InitializeAsync()
+        {
+            await WebView.EnsureCoreWebView2Async(null);
+            WebView.CoreWebView2.DocumentTitleChanged += CoreWebView2_DocumentTitleChanged;
+            WebView.Source = uri;
+        }
+
+        private void CoreWebView2_DocumentTitleChanged(object? sender, object e)
+        {
+            Title = WebView.CoreWebView2.DocumentTitle;
+        }
+    }
+}
