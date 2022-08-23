7898754f3d2346577695820d06d35f113dca09a8
Tetsuya Nishimura <ttynsmr@gmail.com>
2022-08-23 22:55:20 +0900

    apply favicon

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace WebFrame
 {
@@ -31,11 +32,17 @@ namespace WebFrame
             await WebView.EnsureCoreWebView2Async(null);
             WebView.CoreWebView2.DocumentTitleChanged += CoreWebView2_DocumentTitleChanged;
             WebView.Source = uri;
+            WebView.CoreWebView2.FaviconChanged += CoreWebView2_FaviconChanged;
         }
 
         private void CoreWebView2_DocumentTitleChanged(object? sender, object e)
         {
             Title = WebView.CoreWebView2.DocumentTitle;
+        }
+
+        private void CoreWebView2_FaviconChanged(object? sender, object e)
+        {
+            Icon = new BitmapImage(new Uri(WebView.CoreWebView2.FaviconUri));
         }
     }
 }
